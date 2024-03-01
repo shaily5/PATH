429ebbe25e01d0823928bdb67ddd08d7ad3160cc
patelhappy18 <patelhappy9581@gmail.com>
2024-03-01 13:03:04 -0500

    Initial commit

---
+++ car_ride/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarRideConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "car_ride"
---
+++ parcel_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ParcelServiceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "parcel_service"
