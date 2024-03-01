ddc7ee0cb41e026bcfe962f8632e6c4a9dbd7978
patelhappy18 <patelhappy9581@gmail.com>
2024-03-01 13:47:28 -0500

    Login/Registration Model

--- PATH/models.py
+++ PATH/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(null=True, blank=True)
+    # Add more fields as needed
+
+    def __str__(self):
+        return self.email
+
+
