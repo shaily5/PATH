b344d1c83fe9f4da3dd267b2ec9a8b01c6e89bbe
kushank patel <kushankpatel7@gmail.com>
2024-03-01 14:14:05 -0500

    add the car rental model

--- car_rental/models.py
+++ car_rental/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(null=True, blank=True)
+    # Add more fields as needed
+
+    def __str__(self):
+        return self.email
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    daily_rate = models.DecimalField(max_digits=8, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.year} {self.make} {self.model}"
