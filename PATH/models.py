ec5af5758d658007fccefcc1566532b060c5754e
patelhappy18 <patelhappy9581@gmail.com>
2024-03-03 13:11:56 -0500

    Login Authentication

--- PATH/models.py
+++ PATH/models.py
@@ -6,7 +6,7 @@ class CustomUser(models.Model):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    password = models.CharField(max_length=128)
+    password = models.CharField(max_length=128,default='')
     date_of_birth = models.DateField(null=True, blank=True)
     # Add more fields as needed
 
