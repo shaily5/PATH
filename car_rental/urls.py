5ecb49c6fcba1f3da46cdb2e28dce0d3ee92592c
kushank patel <kushankpatel7@gmail.com>
2024-03-05 22:24:47 -0500

    added new view for the rental car

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -4,4 +4,5 @@ from car_rental import views
 
 urlpatterns = [
     path('cars/', views.car_list, name='car-list'),
+    path('rental_reservation_list/', views.rental_reservation_list, name='rental_reservation_list'),
 ]
--- car_rental/views.py
+++ car_rental/views.py
@@ -1,10 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Car
+from .models import Car, RentalReservation
+
 
 # Create your views here.
 def car_list(request):
     cars = Car.objects.all()
     output = ', '.join([str(car) for car in cars])
     return HttpResponse(output)
+
+def rental_reservation_list(request):
+    # Retrieve all rental reservations
+    reservations = RentalReservation.objects.all()
+
+    # Create a string representation of each reservation
+    reservation_strings = [
+        f"Car: {reservation.car}, Customer: {reservation.customer}, Status: {reservation.status}"
+        for reservation in reservations
+    ]
+
+    # Join the reservation strings with line breaks
+    response_body = '\n'.join(reservation_strings)
+
+    # Return the response with the reservation information
+    return HttpResponse(response_body, content_type='text/plain')
