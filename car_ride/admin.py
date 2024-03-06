127ae2ea613de33400e1c4ce0b8a2ff7a1d96fb9
shaily5 <shailyp05@gmail.com>
2024-03-06 00:11:29 -0500

    Ride Model Registered

--- car_ride/admin.py
+++ car_ride/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import RegisterDriver, CarType, Location
+from .models import RegisterDriver, CarType, Location, Ride
 
 admin.site.register(RegisterDriver)
 admin.site.register(CarType)
 admin.site.register(Location)
+admin.site.register(Ride)
