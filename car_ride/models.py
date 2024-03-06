c9d1055cd549fa654455984ebccab2b95b42ea67
Srushty Patel <srushtyp05@gmail.com>
2024-03-05 23:53:24 -0500

    RegisterDriver Model Created

--- car_ride/models.py
+++ car_ride/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+class RegisterDriver(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ]
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=100)
+    confirm_password = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.name
