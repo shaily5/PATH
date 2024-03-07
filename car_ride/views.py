3e0e862e6387882b2cf513a7d72b384f02c660cc
Srushty Patel <srushtyp05@gmail.com>
2024-03-06 23:45:31 -0500

    Created a form for Rideform

--- car_ride/views.py
+++ car_ride/views.py
@@ -2,9 +2,11 @@ from .models import Ride
 from django.contrib import messages
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'car_ride/home.html')
 
 def all_rides(request):
     rides = Ride.objects.all()
     return render(request, 'car_ride/all_rides.html', {'rides':rides})
+
