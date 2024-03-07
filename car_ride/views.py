86008a0f8da2699b3157427d5115f6093232afa7
meet24 <meet241patel@gmail.com>
2024-03-06 23:56:26 -0500

    View created for About Us and Landing

--- car_ride/views.py
+++ car_ride/views.py
@@ -2,7 +2,7 @@ from .models import Ride
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import RideForm
-
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
     return render(request, 'car_ride/home.html')
@@ -21,3 +21,8 @@ def post_ride(request):
         form = RideForm()
     return render(request, 'car_ride/post_ride.html', {'form': form})
 
+def about_us(request):
+    return render(request, 'car_ride/about_us.html')
+
+def landing(request):
+    return render(request, 'landing.html')
