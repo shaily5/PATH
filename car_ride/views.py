bac110030cc4ccbfb31bfc0b37d5513554472c81
Srushty Patel <srushtyp05@gmail.com>
2024-03-06 23:42:03 -0500

    Created home and all_rides views

--- car_ride/views.py
+++ car_ride/views.py
@@ -1,3 +1,10 @@
+from .models import Ride
+from django.contrib import messages
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'car_ride/home.html')
+
+def all_rides(request):
+    rides = Ride.objects.all()
+    return render(request, 'car_ride/all_rides.html', {'rides':rides})
