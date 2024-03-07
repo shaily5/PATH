a6cbf3a03c9f0a0bab90e7b4824d795b6bad62b1
Srushty Patel <srushtyp05@gmail.com>
2024-03-06 23:47:50 -0500

    Created a view for post_ride

--- car_ride/views.py
+++ car_ride/views.py
@@ -1,6 +1,7 @@
 from .models import Ride
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RideForm
 
 
 def home(request):
@@ -9,4 +10,14 @@ def home(request):
 def all_rides(request):
     rides = Ride.objects.all()
     return render(request, 'car_ride/all_rides.html', {'rides':rides})
+
+def post_ride(request):
+    if request.method == 'POST':
+        form = RideForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('all_rides')
+    else:
+        form = RideForm()
+    return render(request, 'car_ride/post_ride.html', {'form': form})
 
