9a49bb89d487d1fb74cba0d5d5b962b8d21d0e71
shaily5 <shailyp05@gmail.com>
2024-03-11 13:15:01 -0400

    ContactUs Model created

--- car_ride/admin.py
+++ car_ride/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Customer
+from .models import Customer, ContactUs
 
 admin.site.register(Customer)
-
+admin.site.register(ContactUs)
--- car_ride/models.py
+++ car_ride/models.py
@@ -15,3 +15,12 @@ class Customer(models.Model):
     def _str_(self):
         return str(self.fname)
 
+class ContactUs(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=80, unique=True, blank=False)
+    phone = models.CharField(max_length=11, null=False, blank=True)
+    msg = models.CharField(max_length=200)
+
+    def _str_(self):
+        return self.name
+
